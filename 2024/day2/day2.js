8553e125b7773b97175fbd69f90f15504319cba1
FL9NS <95050333+fl9ns@users.noreply.github.com>
2025-03-25 03:58:05 +0100

    Update day2.js

--- 2024/day2/day2.js
+++ 2024/day2/day2.js
@@ -38,6 +38,70 @@ fs.readFile('./day2.txt', 'utf8', (err, data) => {
         console.log(`${indecrease} lignes trouvées avec des augmentations et diminutions.`)
         console.log(`${differ} lignes trouvées avec des différences trop importantes.`)
         console.log(`il reste ${safe} lignes sûres.`)
+
+
+
+        console.log("\n"+`_______________`)
+        console.log(`Deuxième partie`+"\n")
+
+        let safe2 = 0
+
+        lines.forEach(line => {
+
+            const numbers = line.split(' ').map(Number)
+
+            let safeStrict = false
+            // un doublon = non sûr
+            // -> si deux doublons se suivent = non sûr
+            // -> si deux doublons ne se suivent pas, c'est une augmentation et diminution = non sûr
+            if(checkDuplicate(numbers) === false) {
+
+                // en hausse ou en baisse, mais sans fluctuation.
+                if(checkIncrease(numbers) === true || checkDecrease(numbers) === true ) {
+
+                    // pas d'écart de plus de 3
+                    if(checkDiffer3(numbers) === true) {
+
+                        // déjà sûr
+                        safe2++
+                        safeStrict = true
+                    }
+                }
+            }
+
+            // on vérifie ceux qui ne sont pas sûr
+            // avec la nouvelle règle 
+            if(safeStrict === false) {
+
+                // on enlève un nombre dans le tableau et on retest
+                // autant de fois qu'il y a de nombre dans la ligne
+                for(let i=0; i<numbers.length; i++) {
+                    
+                    let newNumbers = numbers.slice()
+                    newNumbers.splice(i, 1)
+
+                    // un doublon = non sûr
+                    // -> si deux doublons se suivent = non sûr
+                    // -> si deux doublons ne se suivent pas, c'est une augmentation et diminution = non sûr
+                    if(checkDuplicate(newNumbers) === false) {
+
+                        // en hausse ou en baisse, mais sans fluctuation.
+                        if(checkIncrease(newNumbers) === true || checkDecrease(newNumbers) === true ) {
+
+                            // pas d'écart de plus de 3
+                            if(checkDiffer3(newNumbers) === true) {
+
+                                // devient sûr avec un nombre en moins
+                                safe2++
+                                break
+                            }
+                        }
+                    }
+                }
+            }
+        })
+
+        console.log(`il y a ${safe2} lignes sûres avec la nouvelle règle.`)
     }
 })
 
