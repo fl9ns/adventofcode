0937abba9c66eb892557168bde7e856419f8c09b
FL9NS <95050333+fl9ns@users.noreply.github.com>
2025-03-25 04:41:55 +0100

    Update day3.js

--- 2024/day3/day3.js
+++ 2024/day3/day3.js
@@ -3,7 +3,44 @@ const fs = require('node:fs')
 fs.readFile('./day3.txt', 'utf8', (err, data) => {
     if(!err) {
 
-        const mulStart = data.split(`mul(`)
+        console.log(`_______________`)
+        console.log(`Première partie`+"\n")
+
+        console.log(`Total : ${parseMul(data)}`)
+
+        console.log("\n"+`_______________`)
+        console.log(`Deuxième partie`+"\n")
+
+        // on repère les don´t
+        const dontStart = data.split(`don't()`)
+
+        let newTotal = 0
+
+        for(let i=0; i<dontStart.length; i++) {
+
+            // avant le don´t
+            if(i===0) {
+                newTotal += parseMul(dontStart[0])
+            } else {
+
+                // cherche le prochain do
+                const doStart = dontStart[i].split(`do()`)
+                if(doStart.length > 1) {
+
+                    // si y'a plusieurs do()
+                    for(let d=1; d<doStart.length; d++) {
+                        newTotal += parseMul(doStart[d])
+                    }
+                }
+            }
+        }
+
+        console.log(`Nouveau total : ${newTotal}`)
+    }
+})
+
+function parseMul(data) {
+    const mulStart = data.split(`mul(`)
 
         let total = 0
 
@@ -22,7 +59,5 @@ fs.readFile('./day3.txt', 'utf8', (err, data) => {
                 }
             }
         }
-
-        console.log(`Total : ${total}`)
-    }
-})
+        return total
+}
